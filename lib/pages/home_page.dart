df058a7ec1054f1a3afbac2dbe23596191ab0eb4
Abdul Fatah <fatah03111995@gmail.com>
2024-01-31 15:53:52 +0700

    set font size

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:calculatorapp_rep2/components/textsyle.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -20,13 +21,15 @@ class HomePage extends StatelessWidget {
       ),
       body: Center(
         child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-          const SizedBox(
+          SizedBox(
             width: 300,
             child: Center(
               child: Text(
-                'Welcome to this application this is use to calculate the number, includes multiply, divide, minus, plus, and percent',
-                textAlign: TextAlign.center,
-              ),
+                  'Welcome to this application this is use to calculate the number, includes multiply, divide, minus, plus, and percent',
+                  textAlign: TextAlign.center,
+                  style: GoogleFonts.poppins(
+                    textStyle: const TextStyle(fontSize: 24),
+                  )),
             ),
           ),
           const SizedBox(height: 30),
@@ -41,7 +44,7 @@ class HomePage extends StatelessWidget {
                   value: 'Click Here to The App',
                   style: const TextStyle(
                       color: Colors.white,
-                      fontSize: 14,
+                      fontSize: 18,
                       fontWeight: FontWeight.w500)))
         ]),
       ),
