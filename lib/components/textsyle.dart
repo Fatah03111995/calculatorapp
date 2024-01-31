7ee465acbb600a8d4d042f23c252a9a33a4ad3c3
Abdul Fatah <fatah03111995@gmail.com>
2024-01-31 13:10:52 +0700

    homepage_add button and text

--- lib/components/textsyle.dart
+++ lib/components/textsyle.dart
@@ -2,5 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 Widget txtStyle({required String value, TextStyle? style}) {
-  return Text(value, style: GoogleFonts.poppins(textStyle: style));
+  return Text(value,
+      style: GoogleFonts.poppins(
+          textStyle: style ??
+              const TextStyle(fontSize: 24, fontWeight: FontWeight.w500)));
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -11,15 +11,24 @@ class HomePage extends StatelessWidget {
         title: txtStyle(
             value: 'Calculator App',
             style: const TextStyle(
-              fontSize: 32,
-              fontWeight: FontWeight.w600,
+              fontSize: 24,
+              fontWeight: FontWeight.w500,
+              color: Colors.white,
             )),
+        centerTitle: true,
+        backgroundColor: Colors.blue[600],
       ),
       body: Center(
-        child: Column(children: [
-          const Text('''Welcome to this application
-          this is use to calculate the number,
-          includes multiply, divide, minus, plus, and percent'''),
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          const SizedBox(
+            width: 300,
+            child: Center(
+              child: Text(
+                'Welcome to this application this is use to calculate the number, includes multiply, divide, minus, plus, and percent',
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ),
           const SizedBox(height: 30),
           ElevatedButton(
               style: const ButtonStyle(
@@ -31,7 +40,9 @@ class HomePage extends StatelessWidget {
               child: txtStyle(
                   value: 'Click Here to The App',
                   style: const TextStyle(
-                      fontSize: 14, fontWeight: FontWeight.w500)))
+                      color: Colors.white,
+                      fontSize: 14,
+                      fontWeight: FontWeight.w500)))
         ]),
       ),
     );
